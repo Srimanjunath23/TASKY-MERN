5459732f54eef9581f5f5e05627b4c9f4cc2f367
Srimanjunath23 <rsm2304r@gmail.com>
2025-02-20 01:08:33 +0530

    Solved the refresh issue and changed the ui

--- tasky-frontend/src/components/Tasky.js
+++ tasky-frontend/src/components/Tasky.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import {Toaster} from '@blueprintjs/core'
+
+const toast=Toaster.create({
+  position:"top-right"
+})
 
 const Tasky = () => {
   const [title,settitle]=useState("");
   const [description,setdescription]=useState("");
   const [error,seterror]=useState("");
-  const [successmsg,setsuccessmsg]=useState("");
+  // const [successmsg,setsuccessmsg]=useState("");
   const [entireTask,setentireTask]=useState([]);
   const [editid,seteditid]=useState(-1);
   const [edittitle,setedittitle]=useState("")
@@ -12,33 +17,44 @@ const Tasky = () => {
   
   const api="http://localhost:8000"
   const addItem=()=>{
-    if(title.trim()!=="" && description.trim()!==""){
+    if(!title.trim() || !description.trim()){
+      seterror("Title and Description cannot be empty.");
+      setTimeout(()=>seterror(""),3000)
+    }
+    else if(title.trim()!=="" && description.trim()!==""){
       fetch(api+'/tasky',{
         method:"POST",
         headers:{
           'Content-type':'application/json'
         }
         ,body:JSON.stringify({title,description})
-      }).then(
-        (res)=>{
-          if(res.ok){
-            setentireTask([...entireTask,{title,description}]);
-            settitle("")
-            setdescription("")
-            setsuccessmsg("Item added successfuly :)")
-            setTimeout(()=>
-            setsuccessmsg(""),3000)
-           
+      }).then(async (res)=>{
+        const newdata= await res.json();
+        if(res.ok){
+          setentireTask((entireTask)=>[...entireTask,newdata ]);
+          settitle("")
+          setdescription("")
+          toast.show({
+            message:"Item  added successfully",
+            intent:"success",
+            timeout:"3000"
+          });
 
-          }
-          else{
-            seterror("Unable to add Item :(");
-            setTimeout(()=>
-              seterror(""),3000)
+          //setsuccessmsg("Item added successfuly :)")
+          //setTimeout(()=>
+          //setsuccessmsg(""),3000)
+         
 
-          }
         }
-      )
+       
+        else{
+          seterror("Cant able to add item");
+          setTimeout(()=>
+            seterror(""),3000)
+
+        }
+      })
+     
     }
    
 
@@ -76,9 +92,15 @@ const Tasky = () => {
               }
               return item
             })
-            setsuccessmsg("Item updated successfuly :)")
-            setTimeout(()=>
-            setsuccessmsg(""),3000)
+            // setsuccessmsg("Item updated successfuly :)")
+            // setTimeout(()=>
+            // setsuccessmsg(""),3000)
+
+            toast.show({
+              message:"Item  Updated successfully",
+              intent:"success",
+              timeout:"3000"
+            });
             seteditid(-1)
            
 
@@ -101,8 +123,13 @@ const Tasky = () => {
           if (res.ok) {
             // Update the state immediately after successful deletion
             setentireTask((prevTasks) => prevTasks.filter((item) => item._id !== id));
-            setsuccessmsg("Item deleted successfully :)");
-            setTimeout(() => setsuccessmsg(""), 3000);
+            // setsuccessmsg("Item deleted successfully :)");
+            // setTimeout(() => setsuccessmsg(""), 3000);
+            toast.show({
+              message:"Item  Deleted successfully",
+              intent:"success",
+              timeout:"3000"
+            });
           } else {
             seterror("Unable to delete item :(");
             setTimeout(() => seterror(""), 3000);
@@ -118,29 +145,49 @@ const Tasky = () => {
   return (
     
 <>
-<div className='row p-3 bg-primary text-white text-center'>
-  <h1>TaskyTrack</h1>
-</div>
-<br/>
-<div className='row '>
-  <h3>Add Item </h3>
+{/* HEADING */}
+<div className=' row p-2 bg-light text-dark text-center  '>
   
-<div className='input-group d-flex gap-2 '>
-    <input placeholder='Add Title' className='form-control rounded w-25 col-12 col-sm-6' type='text' value={title} onChange={(e)=>settitle(e.target.value)}/>
-    <input placeholder='Add Description' className='form-control rounded w-25 col-12 col-sm-6' type='text' value={description} onChange={(e)=>setdescription(e.target.value)}/>
-    <button className='btn btn-warning rounded col-12 col-sm-auto ' onClick={addItem}>Add Item</button>
-</div>
- {successmsg && <p className='text-success'>{successmsg}</p>} 
- {error && <p className='text-danger'>{error}</p>} 
+  <h1  style={{fontFamily:'serif',fontWeight:'bolder'}} >TaskyTrack</h1>
+    <p className="text-muted-foreground  mt-2">Keep track of your daily tasks</p>
 </div>
 <br/>
 
-<div className='row p-4 ' >
+{/* ADD ITEM */}
+<div className='shadow-lg  bg-warning text-dark row border p-2 rounded m-1'>
+  <h3>Add Item </h3>
+  
+<div className='input-group d-flex gap-2 mb-4'>
+    <input placeholder='Add Title' className='form-control rounded w-25 col-12 col-sm-6' type='text' value={title} onChange={(e)=>settitle(e.target.value)} required/>
+    <input placeholder='Add Description' className='form-control rounded w-25 col-12 col-sm-6' type='text' value={description} onChange={(e)=>setdescription(e.target.value)} />
+    <button className='btn btn-dark text-white rounded col-12 col-sm-auto ' onClick={addItem}>Add Item</button>
+</div>
+{error&& <h5 className='text-danger'>{error}</h5>}
+
+</div>
+<br/>
+
+{/* CARD */}
+<div className='row p-4'>
+<h1>TASKS</h1>
+
+{successmsg && <p className='text-success'>{successmsg}</p>} 
+{/* {error && <p className='text-danger'>{error}</p> } */}
+
+<br/>
   {
     entireTask.map((item)=>
     <div className="col-sm-6 mb-3" >
-    <div className="card h-100">
-      <div className="card-body d-flex flex-column justify-content-between bg-light rounded">
+    <div className="card h-100"  style={{ transition: "transform 0.3s, box-shadow 0.3s" }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.transform = "scale(1.05)";
+          e.currentTarget.style.boxShadow = "0 4px 10px rgba(0, 0, 0, 0.2)";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.transform = "scale(1)";
+          e.currentTarget.style.boxShadow = "none";
+        }} >
+      <div className="card-body d-flex flex-column justify-content-between rounded text-white" style={{ backgroundColor: "rgba(12, 14, 15, 0.8)" }} >
        
           {
           editid === -1 || editid !== item._id ? <>
